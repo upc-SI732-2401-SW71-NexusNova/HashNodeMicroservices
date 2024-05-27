30582b040e5f866c801dcfc2ad4656409a696637
Damarists <u202119605@upc.edu.pe>
2024-05-26 21:47:20 -0500

    Add Microservice Payment

---
+++ FinancialOperations/Payment/Domain/Entities/Payment.cs
@@ -0,0 +1,10 @@
+﻿namespace FinancialOperations.Payment.Domain.Entities;
+
+public class Payment
+{
+    public int Id { get; set; }
+    public float Amount { get; set; }
+    public string Currency { get; set; }
+    public string CardNumber { get; set; }
+    public string CardCvv { get; set; }
+}
